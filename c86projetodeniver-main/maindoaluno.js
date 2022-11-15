f2769c02a44646fbfd49c964c939098b8187ac06
oimeunom <109981363+oimeunom@users.noreply.github.com>
2022-11-15 11:57:22 -0300

    Add files via upload

---
+++ c86projetodeniver-main/maindoaluno.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanva'); //nome do canva
+ var x= document.getElementById("myAudio");// nome do audio
+
+function newImage()
+{
+	
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+	blockImageObject = Img;
+		
+	blockImageObject.scaleToWidth(700);
+	blockImageObject.scaleToHeight(510);
+	blockImageObject.set({
+	top:0,
+	left:0
+	});
+	canvas.add(blockImageObject);
+	});
+	
+}
+
+function playSound(){
+	x.play();
+}
